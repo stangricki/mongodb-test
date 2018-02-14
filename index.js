f1ed1527b6e96652cb3ab261ca57903926efcd55
Filip Stangricki <stangricki@gmail.com>
2018-02-14 07:58:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('port',(process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
